60b2703bf4b5ffbe3a3f451a54bf9eed86f04d3e
developer-mahin <mdmahin1310@gmail.com>
2023-02-19 01:49:09 +0600

    complete the banner tabs design

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -2,30 +2,14 @@ import React, { useRef, useState } from "react";
 import { GiNetworkBars } from "react-icons/gi"
 import { ImSpoonKnife } from "react-icons/im"
 import { AiOutlineCar } from "react-icons/ai"
-import Image from "next/image";
-import Link from "next/link";
-import { TiLocationArrow } from "react-icons/ti"
-import { GrFormClose } from "react-icons/gr"
 import styles from "./Banner.module.css"
+import EatTab from "./EatTab";
+import DriveOrDeliver from "./DriveOrDeliver";
+import RideTab from "./RideTab";
 
 
 const Banner = () => {
   const [tabChange, setTabChange] = useState("Drive or deliver")
-  const [location, setLocation] = useState("")
-  const [destination, setDestination] = useState("")
-
-
-  const locationRef = useRef(null);
-  const locationOnButtonClick = () => {
-    locationRef.current.value = "";
-
-  };
-  const destinationRef = useRef(null);
-  const destinationOnButtonClick = () => {
-    destinationRef.current.value = "";
-
-  };
-
 
   const bannerBackgroundImage = {
     backgroundImage: `url(${(tabChange === "Drive or deliver" && "https://i.ibb.co/Qvtcrf3/ezgif-com-webp-to-jpg.jpg") ||
@@ -63,133 +47,57 @@ const Banner = () => {
     </div>
 
 
-    <div className="lg:w-[576px] w-full bg-white absolute top-16 2xl:left-[16%] xl:left-20 lg:left-18 pt-6">
+    <div className="lg:w-[576px] w-full bg-white absolute lg:top-16 top-0 2xl:left-[16%] xl:left-20 lg:left-18 pt-6">
       <div className="grid lg:grid-cols-3 md:grid-cols-5 grid-cols-3 lg:justify-items-center justify-items-start border-b border-b-gray-100 drop-shadow-sm px-6 pb-6">
-        <div
-          className={styles.driveOrDeliver}
-        >
-          <div className="flex justify-center">
-            <GiNetworkBars
-              className="font-bold lg:text-2xl text-base mb-2 text-gray-900" />
-          </div>
+
+        <div>
           <p
-            onClick={(e) => setTabChange(e.target.innerText)}
-            className="font-medium lg:text-base text-sm leading-5 w-[65px]"
+            onClick={() => setTabChange("Drive or deliver")}
+            className="font-medium lg:text-base text-sm leading-5 w-[65px] flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600 text-center"
           >
+            <GiNetworkBars
+              className="font-bold lg:text-2xl text-base mb-2 text-gray-900">
+            </GiNetworkBars>
             Drive or deliver
           </p>
         </div>
 
-        <div
-          className={styles.driveOrDeliver}
-        // className="text-center text-gray-900 hover:text-gray-600 cursor-pointer"
-        >
-          <div className="flex justify-center">
-            <ImSpoonKnife className="font-bold lg:text-2xl text-base mb-2 text-gray-900" />
-          </div>
+        <div>
           <p
-            onClick={(e) => setTabChange(e.target.innerText)}
-            className="font-medium lg:text-base text-sm">Eat</p>
+            onClick={() => setTabChange("Eat")}
+            className="font-medium lg:text-base text-sm flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600">
+            <ImSpoonKnife
+              className="font-bold lg:text-2xl text-base mb-2 text-gray-900">
+            </ImSpoonKnife>
+            Eat
+          </p>
         </div>
 
-        <div
-          className={styles.driveOrDeliver}
-        // className="text-center text-gray-900 hover:text-gray-600 cursor-pointer"
-        >
-          <div className="flex justify-center">
-            <AiOutlineCar className="font-bold lg:text-2xl text-base mb-2 text-gray-900" />
-          </div>
+        <div>
           <p
-            onClick={(e) => setTabChange(e.target.innerText)}
-            className="font-medium lg:text-base text-sm">Ride</p>
+            onClick={() => setTabChange("Ride")}
+            className="font-medium lg:text-base text-sm flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600">
+            <AiOutlineCar
+              className="font-bold lg:text-2xl text-base mb-2 text-gray-900 ">
+            </AiOutlineCar>
+            Ride
+          </p>
         </div>
       </div>
 
       {
-        tabChange === "Drive or deliver" && <>
-
-          <div className="px-11 pt-11 pb-6">
-            <h2 className="text-[52px] font-bold leading-[64px] py-7">Get in the driver’s seat and get paid</h2>
-            <p>Drive on the platform with the largest network of active riders.</p>
-            <div>
-              <button className="px-6 py-3 rounded-lg bg-gray-900 text-white hover:bg-gray-800 font-medium my-6">Sign up to drive</button>
-            </div>
-            <div className="mb-3">
-              <Link href="/" className={styles.deliveringAnimationButton}>Learn more about driving and delivering</Link>
-            </div>
-          </div>
-        </>
+        tabChange === "Drive or deliver"
+          ?
+          <DriveOrDeliver></DriveOrDeliver>
+          :
+          tabChange === "Eat"
+            ?
+            <EatTab></EatTab>
+            :
+            <RideTab></RideTab>
       }
 
-      {
-        tabChange === "Eat" && <>
-          <div className="px-11 pt-11 pb-6">
-            <h2 className="text-[52px] font-bold leading-[64px] pb-7">Discover delicious eats</h2>
-            <p>Order delivery from restaurants you love.</p>
-            <div className="mt-8 mb-4">
-              <button className="px-6 py-3 rounded-lg bg-gray-900 text-white hover:bg-gray-800 font-medium mr-4">Order now</button>
-              <Link className={styles.ownRestaurantButtonAnimation} href="/">Own a restaurant? Partner with Uber Eats</Link>
-            </div>
-          </div>
-        </>
-      }
-
-      {
-        tabChange === "Ride" && <>
-          <div className="px-11 pt-11 pb-6">
-            <h2 className="text-[52px] font-bold leading-[64px] pb-7">Request a ride now</h2>
-            <div className="relative">
-              <input
-                onChange={(e) => setLocation(e.target.value)}
-                className="w-full py-3.5 pl-10 pr-6 outline-none bg-gray-100"
-                type="text"
-                name="location"
-                value={location}
-                ref={locationRef}
-                id=""
-                placeholder="Enter pickup location"
-              />
-              {
-                location.length ? <>
-                  <GrFormClose
-                    onClick={locationOnButtonClick}
-                    className="absolute right-2 top-3 text-2xl cursor-pointer text-gray-500 hover:text-gray-800" />
-                </> : <>
-                  <TiLocationArrow className="absolute right-2 top-3 text-2xl cursor-pointer text-gray-500 hover:text-gray-800" />
-                </>
-              }
-            </div>
-            <div className="mt-3 relative">
-              <input
-                className="w-full py-3.5 pl-10 pr-6 outline-none bg-gray-100"
-                type="text"
-                onChange={(e) => setDestination(e.target.value)}
-                name="destination"
-                ref={destinationRef}
-                id=""
-                placeholder="Enter destination"
-              />
-              {
-                destination.length > 0 &&
-                <GrFormClose
-                  onClick={destinationOnButtonClick}
-                  className="absolute right-2 top-3 text-2xl cursor-pointer text-gray-500 hover:text-gray-800" />
-
-              }
-            </div>
-            <div className="mt-8">
-              <button className="px-6 py-3 rounded-lg bg-gray-900 text-white hover:bg-gray-800 font-medium my-6 mr-4">Request now</button>
-              <button className="px-6 py-3 rounded-lg bg-gray-100 text-black hover:bg-gray-200 font-medium my-6 mr-4">Schedule for later</button>
-            </div>
-          </div>
-        </>
-      }
     </div>
-
-
-
-
-
   </div>;
 };
 
