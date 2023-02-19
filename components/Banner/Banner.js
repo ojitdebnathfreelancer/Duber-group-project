42b2c9600051eed77d7e4f1306bb9a46c798c89a
developer-mahin <mdmahin1310@gmail.com>
2023-02-19 23:40:20 +0600

    complete the banner section with responsive

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -10,71 +10,124 @@ import Image from "next/image";
 
 const Banner = () => {
   const [tabChange, setTabChange] = useState("Drive or deliver");
+
   useEffect(() => {
     console.log(tabChange);
   }, [tabChange]);
+  
   return (
-    <div className="relative h-[72vh]">
-      <div
-        className={`${
-          tabChange === "Eat" || tabChange === "Ride" ? "hidden" : "block"
-        }`}
-      >
-        <Image
-          fill
-          className=" h-[667px] w-full object-cover"
-          src="https://i.ibb.co/Qvtcrf3/ezgif-com-webp-to-jpg.jpg"
-          alt="Image"
-        />
-      </div>
+    <div className="relative lg:h-[667px] md:h-[336px] h-[270px]">
+      <div className="hidden lg:block">
+        <div
+          className={`${tabChange === "Eat" || tabChange === "Ride" ? "hidden" : "block"
+            }`}
+        >
+          <Image
+            fill
+            className=" h-[667px] w-full object-cover"
+            src="https://i.ibb.co/Qvtcrf3/ezgif-com-webp-to-jpg.jpg"
+            alt="Image"
+          />
+        </div>
 
-      <div
-        className={`${
-          tabChange === "Drive or deliver" || tabChange === "Ride"
+        <div
+          className={`${tabChange === "Drive or deliver" || tabChange === "Ride"
             ? "hidden"
             : "block"
-        }`}
-      >
-        <Image
-          fill
-          className="h-[667px] w-full object-cover"
-          src="https://i.ibb.co/wCx1tY2/ezgif-com-webp-to-jpg-1.jpg"
-          alt=""
-        />
-      </div>
+            }`}
+        >
+          <Image
+            fill
+            className="h-[667px] w-full object-cover"
+            src="https://i.ibb.co/wCx1tY2/ezgif-com-webp-to-jpg-1.jpg"
+            alt=""
+          />
+        </div>
 
-      <div
-        className={`${
-          tabChange === "Drive or deliver" || tabChange === "Eat"
+        <div
+          className={`${tabChange === "Drive or deliver" || tabChange === "Eat"
             ? "hidden"
             : "block"
-        }`}
-      >
-        <Image
-          fill
-          className="h-[667px] w-full object-cover"
-          src="https://i.ibb.co/VNhBCZG/ezgif-com-webp-to-jpg-2.jpg"
-          alt=""
-        />
+            }`}
+        >
+          <Image
+            fill
+            className="h-[667px] w-full object-cover"
+            src="https://i.ibb.co/VNhBCZG/ezgif-com-webp-to-jpg-2.jpg"
+            alt=""
+          />
+        </div>
       </div>
 
-      <div className="lg:w-[576px] w-full bg-white absolute lg:top-16 top-0 2xl:left-[16%] xl:left-20 lg:left-18 pt-6 ">
+
+      <div className="block lg:hidden lg:mt-0 md:mt-[400px] mt-[500px]">
+        <div
+          className={`${tabChange === "Eat" || tabChange === "Ride" ? "hidden" : "block"
+            }`}
+        >
+          <Image
+            fill
+            className="h-[667px] w-full object-cover"
+            src="https://i.ibb.co/Qvtcrf3/ezgif-com-webp-to-jpg.jpg"
+            alt="Image"
+          />
+        </div>
+
+        <div
+          className={`${tabChange === "Drive or deliver" || tabChange === "Ride"
+            ? "hidden"
+            : "block"
+            }`}
+        >
+          <Image
+            fill
+            className="h-[667px] w-full object-cover"
+            src="https://i.ibb.co/wCx1tY2/ezgif-com-webp-to-jpg-1.jpg"
+            alt=""
+          />
+        </div>
+
+        <div
+          className={`${tabChange === "Drive or deliver" || tabChange === "Eat"
+            ? "hidden"
+            : "block"
+            }`}
+        >
+          <Image
+            fill
+            className="h-[667px] w-full object-cover"
+            src="https://i.ibb.co/VNhBCZG/ezgif-com-webp-to-jpg-2.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="lg:w-[576px] w-full bg-white absolute lg:top-16 top-0 2xl:left-[16%] xl:left-20 lg:left-18 pt-6 lg:mt-0 md:-mt-[400px] -mt-[500px]">
         <div className="grid lg:grid-cols-3 md:grid-cols-5 grid-cols-3 lg:justify-items-center justify-items-start border-b border-b-gray-100 drop-shadow-sm px-6 pb-6">
-          <div onClick={() => setTabChange("Drive or deliver")}>
-            <p className="font-medium lg:text-base text-sm leading-5 w-[65px] flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600 text-center">
+          <div
+            onClick={() => setTabChange("Drive or deliver")}
+            className="md:w-auto w-full"
+          >
+            <p className="font-medium lg:text-base text-sm leading-5 md:w-[65px] w-full flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600 text-center">
               <GiNetworkBars className="font-bold lg:text-2xl text-base mb-2 text-gray-900"></GiNetworkBars>
               Drive or deliver
             </p>
           </div>
 
-          <div onClick={() => setTabChange("Eat")}>
+          <div
+            onClick={() => setTabChange("Eat")}
+            className="md:w-auto w-full"
+          >
             <p className="font-medium lg:text-base text-sm flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600">
               <ImSpoonKnife className="font-bold lg:text-2xl text-base mb-2 text-gray-900"></ImSpoonKnife>
               Eat
             </p>
           </div>
 
-          <div onClick={() => setTabChange("Ride")}>
+          <div 
+          onClick={() => setTabChange("Ride")}
+          className="md:w-auto w-full"
+          >
             <p className="font-medium lg:text-base text-sm flex justify-center items-center flex-col cursor-pointer text-gray-900 hover:text-gray-600">
               <AiOutlineCar className="font-bold lg:text-2xl text-base mb-2 text-gray-900 "></AiOutlineCar>
               Ride
