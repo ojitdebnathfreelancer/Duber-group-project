d394271f6c437071b7e904a69e57bcc47fb465ed
Fahim <KhanFahimFaisal@outlook.com>
2023-02-21 11:23:28 +0600

    created signin signup mand rider location selection folders and files

---
+++ pages/signup/drive/vehichleSelection.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignupEmailDriveVehichleSelection = () => {
+  return <div>Here user will select vehichele</div>;
+};
+
+export default SignupEmailDriveVehichleSelection;
---
+++ pages/signup/emailPhone.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmailPhone = () => {
+  return <div>Here user will enter phone number</div>;
+};
+
+export default EmailPhone;
