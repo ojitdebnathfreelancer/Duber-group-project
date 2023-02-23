83879dee6de9dd07064cfd4899644c2331e17e6e
developer-mahin <mdmahin1310@gmail.com>
2023-02-23 19:12:33 +0600

    new branch created

--- pages/signup/drive/location.js
+++ pages/signup/drive/location.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SignupDriveLocation = () => {
-  return <div>Here user will enter drive location and refferal code</div>;
+  return (
+    <div>
+      <div>Here user will enter drive location and refferal code</div>
+    </div>
+  );
 };
 
 export default SignupDriveLocation;
