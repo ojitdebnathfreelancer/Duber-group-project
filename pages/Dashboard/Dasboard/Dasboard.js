5d79ed7ec881e9c4cee546f55aa5a81861f85c06
iftakhar711 <iftakharudiin80@gmail.com>
2023-02-26 18:39:04 +0600

    dashboard 1

--- pages/Dashboard/Dasboard/Dasboard.js
+++ pages/Dashboard/Dasboard/Dasboard.js
@@ -77,7 +77,7 @@ const Dashboard = () => {
                     </li>
                 </ul>
             </div>
-            <div className="h-screen flex-1 p-7">
+            <div className=" h-screen flex-1 p-7">
                 <h1 className="text-2xl font-semibold ">Content</h1>
             </div>
         </div>
